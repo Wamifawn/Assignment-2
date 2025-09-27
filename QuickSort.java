e9f2e949c75ed6ac15df98510ae1850dcf7a0010
Wamifawn <garrison.lazard@my.utsa.edu>
2025-09-27 14:23:52 -0500

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -2,12 +2,34 @@ package pkg;
 
 public class QuickSort {
 
-	
-	public static int[] quickSort(int[] nums) 
-	{	      
-        
-        
-		return null;
-	}
+    int partition(int a[], int low, int high)
+    {
+        int pivot = a[high]; 
+        int i = (low-1);
+        for (int j=low; j<high; j++)
+        {
+            if (a[j] <= pivot)
+            {
+                i++;
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        int temp = a[i+1];
+        a[i+1] = a[high];
+        a[high] = temp;
+        return i+1;
+    }
+    
+    public int[] sort(int a[], int l, int h){
+        if (l < h)
+        {
+            int pi = partition(a, l, h);
+            sort(a, l, pi-1);
+            sort(a, pi+1, h);
+        }
+     return a;  
+    }
 	
 }
